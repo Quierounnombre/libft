10a7669b299a899962f16511fedfeb3c1d768e16
Vicente Garcia Andrade <vicgarci@c2r8s1.42madrid.com>
2022-09-25 05:26:56 +0200

    normi

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -17,27 +17,26 @@ t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 	t_list	*firstpoint;
 	t_list	*lst2;
 
-	if (lst && f && del)
+	if (!lst || !f || !del)
+		return (NULL);
+	lst2 = ft_lstnew(f(lst->content));
+	if (lst2)
 	{
-		lst2 = ft_lstnew(f(lst->content));
-		if (lst2)
+		firstpoint = lst2;
+		lst = lst->next;
+		while (lst)
 		{
-			firstpoint = lst2;
-			lst = lst->next;
-			while (lst)
+			lst2->next = ft_lstnew(f(lst->content));
+			if (!(lst2->next))
 			{
-				lst2->next = ft_lstnew(f(lst->content));
-				if (!(lst2->next))
-				{
-					ft_lstclear(&firstpoint, del);
-					return (NULL);
-				}
-				lst = lst->next;
-				lst2 = lst2->next;
+				ft_lstclear(&firstpoint, del);
+				return (NULL);
 			}
-			lst2->next = NULL;
-			return (firstpoint);
+			lst = lst->next;
+			lst2 = lst2->next;
 		}
+		lst2->next = NULL;
+		return (firstpoint);
 	}
 	return (NULL);
 }
